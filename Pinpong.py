3db4f54459978ff538bc5898c00ccf6cb0f32be9
xSk09x <78428579+xSk09x@users.noreply.github.com>
2021-05-16 17:58:05 +0700

    Update Pinpong.py
    Добавил классы и создал обьекты этих классов. Также начал делать игровой цикл

--- Pinpong.py
+++ Pinpong.py
@@ -1,1 +1,49 @@
-print('Hello')
+from pygame import *
+class GameSprite(sprite.Sprite):
+    def __init__(self,player_image,player_x,player_y,player_speed,wight,height):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image),(wight,height))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image,(self.rect.x,self.rect.y))
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
+back = (200, 255, 255) # цвет фона (background)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+racket1 = Player("racket.png",30, 200, 4, 50, 150)
+racket2 = Player("racket.png",520, 200, 4, 50, 150)
+ball = GameSprite("tenis_ball",200, 200, 4, 50, 50)
+
+speed_x = 3
+speed_y = 3
+
+while game:
+    for i in event.get():
+        if i.type == QUIT:
+            game = False
+    if finish != True:
+        window
