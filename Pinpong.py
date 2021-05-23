be945c755fabf9d0f2c2155de52e1eb99291670d
xSk09x <78428579+xSk09x@users.noreply.github.com>
2021-05-23 18:02:48 +0700

    Newfeathes
    There are some feaches which I have added to this game. This update includes scoretable.

--- Pinpong.py
+++ Pinpong.py
@@ -9,6 +9,9 @@ class GameSprite(sprite.Sprite):
         self.rect.y = player_y
     def reset(self):
         window.blit(self.image,(self.rect.x,self.rect.y))
+    def restart(self):
+        self.rect.x = 200
+        self.rect.y = 200
 class Player(GameSprite):
     def update_r(self):
         keys = key.get_pressed()
@@ -38,8 +41,16 @@ racket1 = Player("racket.png",30, 200, 4, 50, 150)
 racket2 = Player("racket.png",520, 200, 4, 50, 150)
 ball = GameSprite("tenis_ball.png",200, 200, 4, 50, 50)
 
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
 speed_x = 3
 speed_y = 3
+score_1 = 0
+score_2 = 0
+
+
 
 while game:
     for i in event.get():
@@ -51,19 +62,34 @@ while game:
         racket2.update_r()
         ball.rect.x += speed_x
         ball.rect.y += speed_y
+        score1=font.render(str(score_1),True,(0, 0, 0))
+        score2=font.render(str(score_2),True,(0, 0, 0))
+        window.blit(score1,(100,50))
+        window.blit(score2,(500,50))
         if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
             speed_x *= -1
             speed_y *= 1
         if ball.rect.y > win_height-50 or ball.rect.y < 0:
             speed_y *= -1
+        # if ball.rect.x < 0:
+        #     finish = True
+        # if ball.rect.x > win_width:
+        #     finish = True
         if ball.rect.x < 0:
-            finish = True
-            game_over = True
+           ball.restart()
+           score_2+=1
+           #window.blit(lose1, (200, 200))
         if ball.rect.x > win_width:
-            finish = True
-            game_over = True
+           ball.restart()
+           score_1+=1
+           #window.blit(lose2, (200, 200))
         racket1.reset()
         racket2.reset()
         ball.reset()
+    else:
+        keys = key.get_pressed() 
+        if keys[K_KP_ENTER]:
+            ball.restart()
+            finish = False
     display.update()
     clock.tick(FPS)
